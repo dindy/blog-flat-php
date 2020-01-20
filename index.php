5460597265cba26836262df71c9a8ca739875982
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-01-20 10:12:31 +0100

    Etape 1

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$connection = new PDO("mysql:host=localhost;dbname=blog_db", 'root', '');
+
+$result = $connection->query('SELECT id, title FROM posts');
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>List of Posts</title>
+    </head>
+    <body>
+        <h1>List of Posts</h1>
+        <ul>
+            <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
+            <li>
+                <a href="/show.php?id=<?php echo $row['id'] ?>">
+                    <?php echo $row['title'] ?>
+                </a>
+            </li>
+            <?php endwhile ?>
+        </ul>
+    </body>
+</html>
+
+<?php
+$connection = null;
+?>
