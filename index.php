ce252ddaa8276490eff50c84dcad24091cad0c2f
Sylvain Saint-André <sylvain.saint-andre@laposte.net>
2020-01-20 10:36:34 +0100

    Etape 2

--- index.php
+++ index.php
@@ -3,27 +3,12 @@ $connection = new PDO("mysql:host=localhost;dbname=blog_db", 'root', '');
 
 $result = $connection->query('SELECT id, title FROM posts');
 
-?>
+$posts = [];
+while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+    $posts[] = $row;
+}
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>List of Posts</title>
-    </head>
-    <body>
-        <h1>List of Posts</h1>
-        <ul>
-            <?php while ($row = $result->fetch(PDO::FETCH_ASSOC)): ?>
-            <li>
-                <a href="/show.php?id=<?php echo $row['id'] ?>">
-                    <?php echo $row['title'] ?>
-                </a>
-            </li>
-            <?php endwhile ?>
-        </ul>
-    </body>
-</html>
-
-<?php
 $connection = null;
-?>
+
+// include the HTML presentation code
+require 'templates/list.php';
